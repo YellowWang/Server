615f13e0295d941a8d70b2a4d3b6ce198550bfd2
Yellow Wang <ericsson2310@yahoo.com.cn>
2012-03-06 01:34:28 +0800

    block I/O tcp example

---
+++ correspondence/correspondence.cpp
@@ -0,0 +1,45 @@
+// block example
+// client
+
+#include "stdafx.h"
+#include "winsock2.h"
+
+#pragma comment(lib,"ws2_32.lib")
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int port = 9999;
+
+	WSADATA wsa;
+	if(WSAStartup(MAKEWORD(2,2),&wsa)!=0){ 
+
+		printf("Ì×½Ó×Ö³õÊ¼»¯Ê§°Ü!");
+
+	}
+
+	SOCKET sockClient;
+	if ((sockClient=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)) < 0)
+	{
+		return false;
+	}
+
+	struct sockaddr_in serverAddr;
+	memset(&serverAddr, 0, sizeof(sockaddr_in));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port	  = htons(port);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+// 	if ((serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1")) == INADDR_NONE)
+// 	{
+// 		return false;
+// 	}
+	
+	if (connect(sockClient, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		return false;
+	}
+
+	WSACleanup();
+	return 0;
+}
+
---
+++ tcp_server1/tcp_server1.cpp
@@ -0,0 +1,65 @@
+// tcp_server1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "winsock2.h"
+
+#pragma comment(lib,"ws2_32.lib")
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int	servSock;
+	int  clentSock;
+	struct  sockaddr_in echoServAddr;
+	struct	sockaddr_in echoClentAddr;
+	int		serverPort;
+	int		clentLen;
+
+	serverPort = 9999;
+	
+	WSADATA wsa;
+	if(WSAStartup(MAKEWORD(2,2),&wsa)!=0){ 
+
+		printf("Ì×½Ó×Ö³õÊ¼»¯Ê§°Ü!");
+
+	}
+
+	if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+	{
+		int err = WSAGetLastError();
+		return false;
+	}
+	
+	memset(&echoServAddr, 0, sizeof(echoServAddr));
+	echoServAddr.sin_family = AF_INET;
+	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	echoServAddr.sin_port		 = htons(serverPort);
+
+	if (bind(servSock, (sockaddr*)&echoServAddr, sizeof(echoServAddr)) < 0)
+	{
+		return false;
+	}
+
+	if (listen(servSock, 5) < 0)
+	{
+		return false;
+	}
+
+	for (;;)
+	{
+		clentLen = sizeof(echoClentAddr);
+		if ((clentSock = accept(servSock, (sockaddr*)&echoClentAddr, &clentLen)) < 0 )
+		{
+			return false;
+		}
+
+		printf("Handling client ip=%s port=%d\n", inet_ntoa(echoClentAddr.sin_addr), 
+			echoClentAddr.sin_port);
+	}
+
+	WSACleanup();
+	return 0;
+}
+
